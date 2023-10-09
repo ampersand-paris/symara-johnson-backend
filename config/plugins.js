2ac009f5b0f98da801a3d8829d1fa1e1cef29eb9
ampersand-paris <ampersand.s.paris@gmail.com>
2023-10-09 10:49:21 -0400

    strapi-rest-cache

--- config/plugins.js
+++ config/plugins.js
@@ -13,4 +13,26 @@ module.exports = ({ env }) => ({
         },
       },
     },
+    "rest-cache": {
+      config: {
+        provider: {
+          name: "memory",
+          options: {
+            max: 32767,
+            maxAge: 3,
+            updateAgeOnGet: false,
+          },
+        },
+        strategy: {
+          debug: true,
+          maxAge: 3600,
+          updateAgeOnGet: false,
+          contentTypes: [
+            // list of Content-Types UID to cache
+            { contentType: "api::about.about", maxAge: 60*60*100 },
+            { contentType: "api::project.project", maxAge: 60*60*100 },
+          ],
+        },
+      },
+    },
   });
